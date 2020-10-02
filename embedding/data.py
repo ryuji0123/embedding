85d1abf184049c31379c97cc4fcb5f7471ddf5da
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-02 17:02:01 +0900

    add data class

--- embedding/data.py
+++ embedding/data.py
@@ -1,15 +1,15 @@
 import pandas as pd
+from abc import ABCMeta, abstractmethod
 from os import path
 from os.path import join
 
 DATA_PATH = join(path.sep, 'workspace', 'data')
 
 
-class Data:
-    def __init__(self, file_path, file_sep=','):
-        self.df = pd.read_csv(file_path, sep=file_sep)
+class Data(metaclass=ABCMeta):
+    def __init__(self, file_path, cols, file_sep=','):
+        self.df = pd.read_csv(file_path, usecols=cols, sep=file_sep)
 
-
-if __name__ == '__main__':
-    file_path = join(DATA_PATH, 'pokemon.csv.gz')
-    data = Data(file_path)
+    @abstractmethod
+    def reduction(self):
+        pass
