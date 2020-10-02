8704cd05002950b9af301b75f670c369d96fc579
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-02 17:33:12 +0900

    add pytest

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -1,4 +1,4 @@
-from embedding.data import Data
+from embedding.data import Data, DATA_PATH
 from embedding.t_sne import TSNEData
 
-__all__ = ["Data", "TSNEData"]
+__all__ = ["Data", "DATA_PATH", "TSNEData"]
