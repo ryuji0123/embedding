3a769759c7a7adf0d183ec0d5d11f2f739c80c0e
stash <39332124+stash-196@users.noreply.github.com>
2020-10-06 13:56:30 +0900

    correct argument from value to variable

--- embedding/locally_linear_embedding.py
+++ embedding/locally_linear_embedding.py
@@ -4,5 +4,5 @@ from sklearn.manifold import LocallyLinearEmbedding
 class LLEData(Data):
     def reduction(self, dim=2):
         # variable name 'embedding' in accordance with source code example
-        embedding = LocallyLinearEmbedding(n_components=2)
+        embedding = LocallyLinearEmbedding(n_components=dim)
         self.em = embedding.fit_transform(self.df)
