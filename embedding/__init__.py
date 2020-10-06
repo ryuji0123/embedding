39e31e356ba51c725b2d81513548d15b8a72b953
stash <ratatouille.apple@gmail.com>
2020-10-06 11:28:32 +0900

    modify __init__.py, add kernel_pca.py

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -1,4 +1,5 @@
 from embedding.data import Data, DATA_PATH
 from embedding.t_sne import TSNEData
+from embedding.kernel_pca import KernelPCAData
 
-__all__ = ["Data", "DATA_PATH", "TSNEData"]
+__all__ = ["Data", "DATA_PATH", "TSNEData", "KernelPCAData"]
