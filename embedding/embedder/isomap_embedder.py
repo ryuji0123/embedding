5a59141cc0b2300707c2922d0252f8ffd5334511
Akiiiira <nan10castle@fuji.waseda.jp>
2020-10-09 17:42:28 +0900

    update isomap

--- embedding/embedder/isomap_embedder.py
+++ embedding/embedder/isomap_embedder.py
@@ -3,5 +3,8 @@ from embedding.embedder import ParentEmbedder
 
 
 class IsomapEmbedder(ParentEmbedder):
-    def embed(self, dim=2, neighbor=5):
+    def __init__(self, *args):
+        super(IsomapEmbedder, self).__init__(*args)
+        self.class_key = "isomap_embedder"
+    def execEmbed(self, dim=2, neighbor=5):
         self.em = Isomap(n_components=dim, n_neighbors=neighbor).fit_transform(self.df)
