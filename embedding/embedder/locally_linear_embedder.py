390eded7ac76da03ba72f3faee343392b1f6d6c2
stash <ratatouille.apple@gmail.com>
2020-10-13 10:54:25 +0900

    update embedder and test

---
+++ embedding/embedder/locally_linear_embedder.py
@@ -0,0 +1,11 @@
+from sklearn.manifold import LocallyLinearEmbedding
+
+from embedding.embedder import ParentEmbedder
+
+class LocallyLinearEmbedder(ParentEmbedder):
+    def __init__(self, *args):
+        super(LocallyLinearEmbedder,self).__init__(*args)
+        self.class_key = "locally_linear_embedder"
+
+    def execEmbed(self, dim=2):
+        self.em = LocallyLinearEmbedding(n_components=dim).fit_transform(self.df)
--- embedding/locally_linear_embedding.py
+++
@@ -1,8 +0,0 @@
-from embedding import Data
-from sklearn.manifold import LocallyLinearEmbedding
-
-class LLEData(Data):
-    def reduction(self, dim=2):
-        # variable name 'embedding' in accordance with source code example
-        embedding = LocallyLinearEmbedding(n_components=dim)
-        self.em = embedding.fit_transform(self.df)
