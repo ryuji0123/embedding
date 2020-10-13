3726e2f04e41e369366cedfd450b1c77c873d9c2
stash <ratatouille.apple@gmail.com>
2020-10-13 10:35:35 +0900

    modify test_kernel_pca

--- tests/test_kernel_pca.py
+++ tests/test_kernel_pca.py
@@ -1,12 +1,11 @@
-from os.path import join
-
-from embedding import DATA_PATH, KernelPCAData
+from embedding.data import chooseData
+from embedding.embedder import KernelPCAEmbedder
 
 
 def test_kernel_pca():
-    tsne = KernelPCAData(join(DATA_PATH, 'pokemon.csv.gz'), cols=['hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed'])
-    tsne.reduction()
-    assert tsne.em.shape == (801, 2)
+    kernel_pca = KernelPCAEmbedder(chooseData("pokemon"))
+    kernel_pca.embed()
+    assert kernel_pca.em.shape == (801, 2)
 
 
 if __name__ == "__main__":
