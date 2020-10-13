05d0c8f386f62d8c6653971c19d0d6bd137a985b
stash <ratatouille.apple@gmail.com>
2020-10-13 15:06:09 +0900

    follow ./build.sh instructions

--- embedding/reducer/nmf_reducer.py
+++ embedding/reducer/nmf_reducer.py
@@ -1,6 +1,7 @@
 from sklearn.decomposition import NMF
 from embedding.reducer import ParentReducer
 
+
 class NMFReducer(ParentReducer):
     def __init__(self, *args):
         super(NMFReducer, self).__init__(*args)
@@ -8,4 +9,3 @@ class NMFReducer(ParentReducer):
 
     def execReduce(self, dim=2):
         self.rd = NMF(n_components=dim, init='random', random_state=0).fit_transform(self.df)
-
