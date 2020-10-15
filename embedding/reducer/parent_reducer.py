b3a36320ee63c46416136aef9751d2c09056301d
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-15 16:42:15 +0900

    update error messages

--- embedding/reducer/parent_reducer.py
+++ embedding/reducer/parent_reducer.py
@@ -9,7 +9,7 @@ from embedding.data import ParentData
 class ParentReducer(metaclass=ABCMeta):
     def __init__(self, data, df=None):
         if not isinstance(data, ParentData):
-            raise NotImplementedError("f{type(data)} should inherit {ParentData}")
+            raise ValueError(f"{type(data)} should inherit {ParentData}")
         if df is None:
             self.data = data
             self.df = data.df
@@ -17,7 +17,7 @@ class ParentReducer(metaclass=ABCMeta):
             self.data = data
             self.df = pd.DataFrame(df)
         else:
-            raise NotImplementedError(f"{type(df)} should be None or {type(np.ndarray)}")
+            raise ValueError(f"{type(df)} should be None or {type(np.ndarray)}")
 
     def reduce(self):
         if self.data.exists(self.class_key):
