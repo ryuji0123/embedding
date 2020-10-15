977862d8b32d8ba273d7ba8001b9c3edc450e540
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-15 17:06:22 +0900

    update embed function to pass **kwargs

--- embedding/embedder/parent_embedder.py
+++ embedding/embedder/parent_embedder.py
@@ -6,11 +6,11 @@ class ParentEmbedder(metaclass=ABCMeta):
         self.data = data
         self.df = data.df
 
-    def embed(self):
+    def embed(self, **kwargs):
         if self.data.exists(self.class_key):
             self.em = self.data.getResult(self.class_key)
         else:
-            self.execEmbed()
+            self.execEmbed(**kwargs)
             self.data.save(self.class_key, self.em)
 
     @abstractmethod
