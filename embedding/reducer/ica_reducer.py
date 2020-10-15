43531fad14f3e090afa011a3f35aa5c104e1917f
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-15 14:53:15 +0900

    change function's param

--- embedding/reducer/ica_reducer.py
+++ embedding/reducer/ica_reducer.py
@@ -8,7 +8,7 @@ class ICAReducer(ParentReducer):
         super(ICAReducer, self).__init__(*args)
         self.class_key = "ica_reducer"
 
-    def execReduce(self, dim=2):
+    def execReduce(self, dim=2, axis=1):
         #  Fast ICA need to normalize by myself.
-        data = self.df - np.mean(np.array(self.df), axis=1)[:, np.newaxis]
+        data = self.df - np.mean(np.array(self.df), axis=axis)[:, np.newaxis]
         self.rd = FastICA(n_components=dim).fit_transform(data)
