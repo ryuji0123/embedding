3998338d2b05bfc8ba752673e8ea55f317dffcf6
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-15 14:23:17 +0900

    update __init__.py

--- embedding/embedder/__init__.py
+++ embedding/embedder/__init__.py
@@ -1,5 +1,5 @@
+from embedding.embedder.locally_linear_embedder import LocallyLinearEmbedder
 from embedding.embedder.parent_embedder import ParentEmbedder
 from embedding.embedder.tsne_embedder import TSNEEmbedder
-from embedding.embedder.locally_linear_embedder import LocallyLinearEmbedder
 
-__all__ = ["TSNEEmbedder", "ParentEmbedder", "LocallyLinearEmbedder"]
+__all__ = ["LocallyLinearEmbedder", "ParentEmbedder", "TSNEEmbedder"]
