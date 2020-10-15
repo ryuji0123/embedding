ce0a0fd3044ae2db38cdc73424adc401f9dabb8a
stash <ratatouille.apple@gmail.com>
2020-10-15 13:44:59 +0900

    update embedding/reducer/__init__.py

--- embedding/reducer/__init__.py
+++ embedding/reducer/__init__.py
@@ -1,4 +1,6 @@
-from embedding.reducer.parent_reducer import ParentReducer
+from embedding.reducer.mds_reducer import MDSReducer
 from embedding.reducer.nmf_reducer import NMFReducer
+from embedding.reducer.pca_reducer import PCAReducer
+from embedding.reducer.parent_reducer import ParentReducer
 
-__all__ = ["ParentReducer", "NMFReducer"]
+__all__ = ["MDSReducer", "NMFReducer", "ParentReducer", "PCAReducer"]
