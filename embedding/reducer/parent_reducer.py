352017a574567a8e66bea5660460076a5fbbb66d
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-15 16:39:49 +0900

    change ParentReducer's init function

--- embedding/reducer/parent_reducer.py
+++ embedding/reducer/parent_reducer.py
@@ -1,10 +1,23 @@
+import numpy as np
+import pandas as pd
+
 from abc import ABCMeta, abstractmethod
+
+from embedding.data import ParentData
 
 
 class ParentReducer(metaclass=ABCMeta):
-    def __init__(self, data):
-        self.data = data
-        self.df = data.df
+    def __init__(self, data, df=None):
+        if not isinstance(data, ParentData):
+            raise NotImplementedError("f{type(data)} should inherit {ParentData}")
+        if df is None:
+            self.data = data
+            self.df = data.df
+        elif isinstance(df, np.ndarray):
+            self.data = data
+            self.df = pd.DataFrame(df)
+        else:
+            raise NotImplementedError(f"{type(df)} should be None or {type(np.ndarray)}")
 
     def reduce(self):
         if self.data.exists(self.class_key):
