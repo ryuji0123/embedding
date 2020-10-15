f6eb60deed46fa51db4dccdd970308959985c697
stash <ratatouille.apple@gmail.com>
2020-10-15 13:42:37 +0900

    update embedding/embedder/__init__.py

--- embedding/embedder/__init__.py
+++ embedding/embedder/__init__.py
@@ -1,4 +1,5 @@
 from embedding.embedder.parent_embedder import ParentEmbedder
 from embedding.embedder.tsne_embedder import TSNEEmbedder
+from embedding.embedder.locally_linear_embedder import LocallyLinearEmbedder
 
-__all__ = ["TSNEEmbedder", "ParentEmbedder"]
+__all__ = ["TSNEEmbedder", "ParentEmbedder", "LocallyLinearEmbedder"]
