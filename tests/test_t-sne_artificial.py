ddba01393ad7c4d7f3510a087ba2f91f9686d6c4
stash <ratatouille.apple@gmail.com>
2020-10-15 20:07:16 +0900

    modify test artificial

--- tests/test_t-sne_artificial.py
+++ tests/test_t-sne_artificial.py
@@ -2,17 +2,13 @@ from embedding.embedder import TSNEEmbedder
 from embedding.data import chooseData
 
 
-def test_t_sne():
+def test_t_sne_artificial():
     tsne = TSNEEmbedder(chooseData("artificial"))
-    tsne2 = TSNEEmbedder(chooseData("pokemon"))
     print(tsne.df)
-    print(tsne2.df)
     tsne.embed()
-    tsne2.embed()
     print(tsne.em)
-    print(tsne2.em)
     assert tsne.em.shape == (801, 2)
 
 
 if __name__ == "__main__":
-    test_t_sne()
+    test_t_sne_artificial()
