d04f6e48dcb604d0a0cc09d580b72abd49799331
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-16 16:59:08 +0900

    cerate project

---
+++ app/embedding_demo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmbeddingDemoConfig(AppConfig):
+    name = 'embedding_demo'
---
+++ app/runserver.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker exec research_embedding python /workspace/app/manage.py runserver 0.0.0.0:8000
