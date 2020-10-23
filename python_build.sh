681f1b4ce432a5f49c87e08cdea80e3b27159202
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-23 16:09:31 +0900

    modify pytest options

--- python_build.sh
+++ python_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude app/,notebooks/
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude app/,notebooks/
-pytest --ignore app/,notebooks/
+pytest --log-cli-level=10 --durations=0 --ignore app/,notebooks/
