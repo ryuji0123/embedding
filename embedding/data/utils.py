f369fed596be4b2a4d441deedca4e3e204a20480
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-23 14:55:32 +0900

    modify embed/reduce function

--- embedding/data/utils.py
+++ embedding/data/utils.py
@@ -1,4 +1,4 @@
-from embedding.data import DATA_PATH, RESULT_PATH, PokemonData, ArtificialData
+from embedding.data import DATA_PATH, CACHE_PATH, PokemonData, ArtificialData
 
 ref = {
         "pokemon": PokemonData,
@@ -9,4 +9,4 @@ ref = {
 def chooseData(key):
     if key not in ref:
         raise NotImplementedError(f"{key} is not supported")
-    return ref[key](DATA_PATH, RESULT_PATH)
+    return ref[key](DATA_PATH, CACHE_PATH)
