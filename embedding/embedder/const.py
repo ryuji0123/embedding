8ac1aeb89b845cdd9a69bddeac47ef0fb81adbfc
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-05 17:51:07 +0900

    add embedders_ref and choose_embedder

--- embedding/embedder/const.py
+++ embedding/embedder/const.py
@@ -3,16 +3,14 @@ from embedding.embedder.kernel_pca_embedder import KernelPCAEmbedder
 from embedding.embedder.laplacian_eigenmaps_embedder import Laplacian_EigenmapsEmbedder
 from embedding.embedder.locally_linear_embedder import LocallyLinearEmbedder
 from embedding.embedder.n_mds_embedder import N_MDSEmbedder
-from embedding.embedder.parent_embedder import ParentEmbedder
 from embedding.embedder.tsne_embedder import TSNEEmbedder
 
-from embedding.embedder.const import EMBEDDERS_REF
-from embedding.embedder.utils import chooseEmbedder
 
-
-__all__ = [
-        "IsomapEmbedder", "KernelPCAEmbedder",
-        "Laplacian_EigenmapsEmbedder", "LocallyLinearEmbedder",
-        "N_MDSEmbedder", "ParentEmbedder", "TSNEEmbedder",
-        "EMBEDDERS_REF", "chooseEmbedder",
-        ]
+EMBEDDERS_REF = {
+        "isomap": IsomapEmbedder,
+        "kernel_pca": KernelPCAEmbedder,
+        "laplacian_eigenmaps": Laplacian_EigenmapsEmbedder,
+        "locally_linear": LocallyLinearEmbedder,
+        "n_mds": N_MDSEmbedder,
+        "t_sne": TSNEEmbedder,
+        }
