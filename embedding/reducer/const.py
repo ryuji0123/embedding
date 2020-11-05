ea8f8852db050adca2dc7d646849f72e12fec587
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-05 17:55:58 +0900

    delete nmf from reducers_ref

--- embedding/reducer/const.py
+++ embedding/reducer/const.py
@@ -1,12 +1,10 @@
 from embedding.reducer.ica_reducer import ICAReducer
 from embedding.reducer.mds_reducer import MDSReducer
-from embedding.reducer.nmf_reducer import NMFReducer
 from embedding.reducer.pca_reducer import PCAReducer
 
 
 REDUCERS_REF = {
         "ica": ICAReducer,
         "mds": MDSReducer,
-        "nmf": NMFReducer,
         "pca": PCAReducer,
         }
