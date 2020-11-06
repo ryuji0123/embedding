f95c757b9a09194f835182eeed6f831d07340085
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-06 12:07:29 +0900

    change form's choices

--- app/demo/forms.py
+++ app/demo/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 
+from embedding.data import DATA_REF
 from embedding.embedder import EMBEDDERS_REF
 from embedding.reducer import REDUCERS_REF
 
 label_texts_before_choices = [
+        "Data",
 	"Embedder",
 	"Reducer",
 	]
 
 class ChoiceForm(forms.Form):
+    data_choice = forms.fields.ChoiceField(
+        choices = (
+            (k, k) for k in DATA_REF.keys()
+        ),
+        required=True,
+        widget=forms.widgets.Select(attrs={"class": "form-control"}) 
+    )
+
     embedder_choice = forms.fields.ChoiceField(
         choices = (
             (k, k) for k in EMBEDDERS_REF.keys()
