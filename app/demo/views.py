9d6b8bdc7b6075512525913ed0653f37a4e0f8f3
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-06 12:07:29 +0900

    change form's choices

--- app/demo/views.py
+++ app/demo/views.py
@@ -13,12 +13,13 @@ def index(request):
     View function for index page,
     """
     fig = getFigure(
-           data_key='pokemon',
+           data_key=request.POST.get("data_choice", "pokemon"),
            embedder_key=request.POST.get("embedder_choice", "isomap"),
            reducer_key=request.POST.get("reducer_choice", "ica"),
            )
 
     forms = ChoiceForm(initial={
+        "data_choice": request.POST.get("data_choice", ""),
         "embedder_choice": request.POST.get("embedder_choice", ""),
         "reducer_choice": request.POST.get("reducer_choice", ""),
         })
