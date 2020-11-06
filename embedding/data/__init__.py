526ffe2c43597b8768a6367316381e82bf005ad9
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-06 12:04:43 +0900

    change where to define DATA_REF

--- embedding/data/__init__.py
+++ embedding/data/__init__.py
@@ -1,10 +1,10 @@
-from embedding.data.const import DATA_PATH, CACHE_PATH
+from embedding.data.const import CACHE_PATH, DATA_PATH, DATA_REF
+from embedding.data.utils import chooseData
 from embedding.data.parent_data import ParentData
 from embedding.data.pokemon_data import PokemonData
 from embedding.data.artificial_data import ArtificialData
 from embedding.data.scurve_data import ScurveData
 from embedding.data.swissroll_data import SwrollData
-from embedding.data.utils import chooseData
 
 
-__all__ = ["DATA_PATH", "CACHE_PATH", "ParentData", "ArtificialData", "PokemonData", "ScurveData", "SwrollData", "chooseData"]
+__all__ = ["CACHE_PATH", "DATA_PATH", "DATA_REF", "ParentData", "ArtificialData", "PokemonData", "ScurveData", "SwrollData", "chooseData"]
--- embedding/data/const.py
+++ embedding/data/const.py
@@ -1,5 +1,17 @@
 from os import path
 from os.path import join
 
+from embedding.data.pokemon_data import PokemonData
+from embedding.data.artificial_data import ArtificialData
+from embedding.data.scurve_data import ScurveData
+from embedding.data.swissroll_data import SwrollData
+
+
 DATA_PATH = join(path.sep, 'workspace', 'data')
 CACHE_PATH = join(path.sep, 'workspace', 'cache')
+DATA_REF = {
+        "pokemon": PokemonData,
+        "artificial": ArtificialData,
+        "scurve": ScurveData,
+        "swissroll": SwrollData
+        }
