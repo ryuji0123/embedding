610bca59eff5c20ac687d0759c6f06edf51c2749
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-06 12:04:43 +0900

    change where to define DATA_REF

--- embedding/data/utils.py
+++ embedding/data/utils.py
@@ -1,14 +1,7 @@
-from embedding.data import DATA_PATH, CACHE_PATH, PokemonData, ArtificialData, ScurveData, SwrollData
-
-ref = {
-        "pokemon": PokemonData,
-        "artificial": ArtificialData,
-        "scurve": ScurveData,
-        "swissroll": SwrollData
-        }
+from embedding.data.const import CACHE_PATH, DATA_PATH, DATA_REF
 
 
 def chooseData(key):
-    if key not in ref:
+    if key not in DATA_REF:
         raise NotImplementedError(f"{key} is not supported")
-    return ref[key](DATA_PATH, CACHE_PATH)
+    return DATA_REF[key](DATA_PATH, CACHE_PATH)
