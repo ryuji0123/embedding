ec2e43db1bffa7d1973c8ef61e9a42bb0e33f06c
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-13 17:54:13 +0900

    ignore facebook_poincare_embeddings

--- python_build.sh
+++ python_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude app/,notebooks/
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude app/,notebooks/
-pytest --log-cli-level=10 --durations=0 --ignore app/,notebooks/
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude app/,notebooks/,facebook_poincare_embeddings/
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude app/,notebooks/,facebook_poincare_embeddings/
+pytest --log-cli-level=10 --durations=0 --ignore app/,notebooks/,facebook_poincare_embeddings/
