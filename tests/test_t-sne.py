92555a1f2a04a2f48ddc1d44774ac7d40d0e8b93
Akira Kodama <akira@vulcanus.murata.eb.waseda.ac.jp>
2020-11-16 17:31:27 +0900

    modified test_t-sne.py for clustered scurve and swissroll

--- tests/test_t-sne.py
+++ tests/test_t-sne.py
@@ -20,6 +20,28 @@ def test_t_sne_artificial():
     assert tsne.em.shape == (801, 2)
 
 
+def test_t_sne_clusteredscurve():
+    logging.info("start t-sne test using clustered-scurve data")
+    logging.info("start generating data")
+    tsne = TSNEEmbedder(chooseData("clustered-scurve"))
+    logging.info("start embedding")
+    tsne.embed(use_cache=True)
+    logging.info("finish all process")
+    assert tsne.em.shape == (10000, 2)
+
+
+def test_t_sne_clusteredswissroll():
+    logging.info("start t-sne test using clustered-swissroll data")
+    logging.info("start generating data")
+    tsne = TSNEEmbedder(chooseData("clustered-swissroll"))
+    logging.info("start embedding")
+    tsne.embed(use_cache=True)
+    logging.info("finish all process")
+    assert tsne.em.shape == (10000, 2)
+
+
 if __name__ == "__main__":
     test_t_sne_pokemon()
     test_t_sne_artificial()
+    test_t_sne_clusteredscurve()
+    test_t_sne_clusteredswissroll()
