32177a8807a46f1eaf671589c19ce0495a755bd8
Akiiiira <nan10castle@fuji.waseda.jp>
2020-11-19 18:50:50 +0900

    modified clustered_swissroll_data.py

--- embedding/data/clustered_swissroll_data.py
+++ embedding/data/clustered_swissroll_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 from embedding.data.parent_data import ParentData
-from embedding.data.synthesizer import ClusteredScurveSynthesizer
+from embedding.data.synthesizer import ClusteredSwissrollSynthesizer
 
 
 class ClusteredSwissrollData(ParentData):
@@ -12,7 +12,7 @@ class ClusteredSwissrollData(ParentData):
         self.data_key = "clustered_swissroll"
 
     def setDataFrameAndColor(self):
-        synthesizer = ClusteredScurveSynthesizer()
+        synthesizer = ClusteredSwissrollSynthesizer()
         data, self.color = synthesizer.synthesizeData(n_dim=3, n_cluster=5, n_points=1000)
         data += abs(np.min(data))
         self.df = pd.DataFrame(data=data, columns=['col0', 'col1', 'col2'])
