92d47d5b0a3ce4a9732dc4855f480c352f4e708e
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-19 16:49:40 +0900

    add synthesizer module

---
+++ embedding/data/synthesizer/__init__.py
@@ -0,0 +1,6 @@
+from embedding.data.synthesizer.parent_synthesizer import ParentSynthesizer
+from embedding.data.synthesizer.clustered_scurve_synthesizer import ClusteredScurveSynthesizer
+from embedding.data.synthesizer.clustered_swissroll_synthesizer import ClusteredSwissrollSynthesizer
+
+
+__all__ = ['ParentSynthesizer', 'ClusteredScurveSynthesizer', 'ClusteredSwissrollSynthesizer']
