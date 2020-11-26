fd13090cad49a35dcf1214126f51c7cf4fd8a857
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-26 21:13:18 +0900

    update class keys

--- embedding/embedder/laplacian_eigenmaps_embedder.py
+++ embedding/embedder/laplacian_eigenmaps_embedder.py
@@ -6,7 +6,7 @@ class Laplacian_EigenmapsEmbedder(ParentEmbedder):
 
     def __init__(self, *args):
         super(Laplacian_EigenmapsEmbedder, self).__init__(*args)
-        self.class_key = "lap-e_embedder"
+        self.class_key = "laplacian_eiganmaps_embedder"
 
     def execEmbed(self, dim=2, neighbor=10):
         self.em = SpectralEmbedding(n_components=dim, n_neighbors=neighbor).fit_transform(self.df)
--- embedding/embedder/n_mds_embedder.py
+++ embedding/embedder/n_mds_embedder.py
@@ -6,7 +6,7 @@ class N_MDSEmbedder(ParentEmbedder):
 
     def __init__(self, *args):
         super(N_MDSEmbedder, self).__init__(*args)
-        self.class_key = "n-mds_embedder"
+        self.class_key = "n_mds_embedder"
 
     def execEmbed(self, dim=2, metric=None):
         self.em = MDS(n_components=dim, metric=metric).fit_transform(self.df)
