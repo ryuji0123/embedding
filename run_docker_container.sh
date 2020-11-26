a5442c8f294ac9b8dcd91ef52eb9bbb5cfae6ec4
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-26 14:33:22 +0900

    fix run script's bug

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm -p 8000:8000 -p 18888:18888 -v ~/research-embedding:/workspace -v $(readlink -f ../results/):$(readlink -f ../results/) --name research_embedding embedding bash
+docker run -it --rm -p 8000:8000 -p 18888:18888 -v ~/research-embedding:/workspace -v $(readlink -f ./results/):$(readlink -f ./results/) --name research_embedding embedding bash
