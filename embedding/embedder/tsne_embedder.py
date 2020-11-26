b561a88b006beb8551c67f335285e58fd8aaa90c
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-26 21:13:18 +0900

    update class keys

--- embedding/embedder/tsne_embedder.py
+++ embedding/embedder/tsne_embedder.py
@@ -7,7 +7,7 @@ class TSNEEmbedder(ParentEmbedder):
 
     def __init__(self, *args):
         super(TSNEEmbedder, self).__init__(*args)
-        self.class_key = "t-sne_embedder"
+        self.class_key = "t_sne_embedder"
 
     def execEmbed(self, dim=3):
         self.em = TSNE(n_components=dim).fit_transform(self.df)
