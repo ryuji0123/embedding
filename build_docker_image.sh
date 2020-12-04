1da1c58a482870980922be38c8ddb91b9062c1fa
ryuji0123 <ryo1011.javelin@gmail.com>
2020-12-04 15:50:06 +0900

    change default user and change permissions

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker build -t embedding -f docker/Dockerfile .
+docker build \
+	--build-arg USER_ID=$(id -u) \
+	--build-arg GROUP_ID=$(id -g) \
+	-f docker/Dockerfile \
+	-t embedding \
+	.
