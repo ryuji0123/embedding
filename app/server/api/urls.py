56a732294f83ded780580ab090772a9e72342a4f
ryuji0123 <ryo1011.javelin@gmail.com>
2020-12-04 17:57:53 +0900

    add csv restframework prototype

--- app/server/api/urls.py
+++ app/server/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
 from api import views
+from django.conf.urls import url, include
+from django.urls import path
+from rest_framework import routers
+
+from app.server.api.views import EmbeddingAPIView
 
 app_name = 'api'
+
 urlpatterns = [
-    path('', views.index, name='index'),   # 一覧
+    path('', views.index, name='index'),
+    path('csv', EmbeddingAPIView.as_view(), name='csv')
 ]
