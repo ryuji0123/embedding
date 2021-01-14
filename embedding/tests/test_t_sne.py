532c981d69778131636b32db781763805017de3f
Akiiiira <nan10castle@fuji.waseda.jp>
2021-01-14 15:09:26 +0900

    align basic_cluster data_key

--- embedding/tests/test_t_sne.py
+++ embedding/tests/test_t_sne.py
@@ -8,5 +8,12 @@ def test_t_sne_pokemon():
     assert tsne.em.shape == (801, 2)
 
 
+def test_t_sne_basic_cluster():
+    tsne = TSNEEmbedder(chooseData("basic_cluster"))
+    tsne.embed(use_cache=True, dim=2)
+    assert tsne.em.shape == (801, 2)
+
+
 if __name__ == "__main__":
     test_t_sne_pokemon()
+    test_t_sne_basic_cluster()
