362b17c95c3b6febe57a14a07cdbcab5b35e6df7
ryuji0123 <ryo1011.javelin@gmail.com>
2021-01-22 14:55:33 +0900

    change port numbers

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -2,7 +2,7 @@
 docker run -it --rm \
 	--name research_embedding \
 	-p 8000:8000 \
-	-p 18888:18888 \
+	-p 18885:18885 \
 	-v ~/research-embedding:/workspace \
 	-v $(readlink -f ./results/):$(readlink -f ./results/) \
 	embedding bash
