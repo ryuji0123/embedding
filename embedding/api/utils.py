34f6ba26c6b02a1b3f9466047220d24161a3ab5b
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-05-27 17:03:03 +0900

    [update] Add docstring

--- embedding/api/utils.py
+++ embedding/api/utils.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Utils
+Utility functions for embedding.
+"""
+
+from pandas import DataFrame
+
 from embedding.data import chooseData
 from embedding.embedder import chooseEmbedder
 
 
-def getEmbedding(data_key, embedder_key):
+def get_embedding(data_key: str, embedder_key: str) -> DataFrame:
+    """Get embedding
+    Args:
+        data_key (str): Name of dataset.
+        embedder_key (str): Name of embedder.
+    Returns:
+        DataFrame: Embedding results.
+    """
+
     sc_data = chooseData(data_key)
     embedder = chooseEmbedder(embedder_key, sc_data)
     embedder.embed()
