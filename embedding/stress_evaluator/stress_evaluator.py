b7cd8297774f8cf1bd3620f14c8b3c7351046bee
Akiiiira <nan10castle@fuji.waseda.jp>
2021-07-13 14:58:11 +0900

    add local ranking stress and middle ranking stress

--- embedding/stress_evaluator/stress_evaluator.py
+++ embedding/stress_evaluator/stress_evaluator.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
+import os
 
-from notebooks.embedding.stress_evaluator.kruskal_stress_evaluator import measure_kruskal_stress
+from embedding.stress_evaluator.kruskal_stress_evaluator import measure_kruskal_stress
+from embedding.stress_evaluator.ranking_based_stress import measure_ranking_stress_k_k, \
+    measure_ranking_stress_k_km, measure_ranking_stress_intersectional
 
 
 class StressEvaluator:
 
     def __init__(self, actual_df, n_representative_points=100):
         self.actual_points = convert_dataframe_to_numpy(actual_df)
+        self.local_extra_rank_values = []
+        self.middle_extra_rank_values = []
 
         # representative points assertion
         assert self.actual_points.shape[0] > n_representative_points, \
@@ -24,6 +29,136 @@ class StressEvaluator:
             f'The number of points should be same. actual_data: {self.actual_points.shape}, fitted_data: {fitted_df.shape}'
 
         return measure_kruskal_stress(self.actual_points, convert_dataframe_to_numpy(fitted_df))
+
+    def local_ranking(self, fitted_df, n_neighbors, neighbor_key='k-k+m', weight=1.0, is_z_value=True):
+        # shape assertion
+        assert self.actual_points.shape[0] == fitted_df.shape[0], \
+            f'The number of points should be same. raw_data: {self.actual_points.shape}, fitted_data: {fitted_df.shape}'
+
+        stress = 0
+        if neighbor_key == 'k-k+m':
+            stress, self.local_extra_rank_values = measure_ranking_stress_k_km(self.actual_points,
+                                                                               convert_dataframe_to_numpy(
+                                                                                   fitted_df),
+                                                                               self.n_representative_points,
+                                                                               self.representative_indexes,
+                                                                               n_neighbors, is_z_value, rank_key='local')
+
+        if neighbor_key == 'k-k':
+            stress = measure_ranking_stress_k_k(self.actual_points, convert_dataframe_to_numpy(fitted_df),
+                                                self.n_representative_points, self.representative_indexes,
+                                                n_neighbors, is_z_value, rank_key='local')
+
+        if neighbor_key == 'intersectional':
+            stress = measure_ranking_stress_intersectional(self.actual_points,
+                                                           convert_dataframe_to_numpy(fitted_df),
+                                                           self.n_representative_points,
+                                                           self.representative_indexes,
+                                                           n_neighbors, is_z_value, rank_key='local', weight=weight)
+        if is_z_value:
+            if neighbor_key == 'k-k+m':
+                local_k_km_random_rank_mean, local_k_km_random_rank_var = self.extract_random_rank_distribution_params(
+                    n_neighbors, self.local_extra_rank_values)
+                z_stress = stress_standardize(stress, [local_k_km_random_rank_mean, local_k_km_random_rank_var])
+            else:
+                local_k_k_random_rank_mean, local_k_k_random_rank_var = self.extract_random_rank_distribution_params(
+                    n_neighbors)
+                z_stress = stress_standardize(stress, [local_k_k_random_rank_mean, local_k_k_random_rank_var])
+
+            return z_stress
+
+        return stress
+
+    def middle_ranking(self, fitted_df, n_neighbors, neighbor_key='k-k', weight=1.0, is_z_value=True):
+        # shape assertion
+        assert self.actual_points.shape[0] == fitted_df.shape[0], \
+            f'The number of points should be same. raw_data: {self.actual_points.shape}, fitted_data: {fitted_df.shape}'
+        # n_neighbors assertion
+        assert self.n_representative_points >= n_neighbors, \
+            f'The number of neighbors points should be smaller than number of representative points.' \
+            f'n_representative_points: {self.n_representative_points}, n_neighbors: {n_neighbors}'
+
+        stress = 0
+        if neighbor_key == 'k-k+m':
+            stress, self.middle_extra_rank_values = measure_ranking_stress_k_km(self.actual_points,
+                                                                                convert_dataframe_to_numpy(
+                                                                                    fitted_df),
+                                                                                self.n_representative_points,
+                                                                                self.representative_indexes,
+                                                                                n_neighbors, is_z_value,
+                                                                                rank_key='middle')
+
+        if neighbor_key == 'k-k':
+            stress = measure_ranking_stress_k_k(self.actual_points, convert_dataframe_to_numpy(fitted_df),
+                                                self.n_representative_points, self.representative_indexes,
+                                                n_neighbors, is_z_value, rank_key='middle')
+        if neighbor_key == 'intersectional':
+            stress = measure_ranking_stress_intersectional(self.actual_points,
+                                                           convert_dataframe_to_numpy(fitted_df),
+                                                           self.n_representative_points,
+                                                           self.representative_indexes,
+                                                           n_neighbors, is_z_value, rank_key='middle', weight=weight)
+
+        if is_z_value:
+            if neighbor_key == 'k-k+m':
+                middle_k_km_random_rank_mean, middle_k_km_random_rank_var = self.extract_random_rank_distribution_params(
+                    n_neighbors, self.middle_extra_rank_values)
+                z_stress = stress_standardize(stress, [middle_k_km_random_rank_mean, middle_k_km_random_rank_var])
+            else:
+                middle_k_k_random_rank_mean, middle_k_k_random_rank_var = self.extract_random_rank_distribution_params(
+                    n_neighbors)
+                z_stress = stress_standardize(stress, [middle_k_k_random_rank_mean, middle_k_k_random_rank_var])
+
+            return z_stress
+
+        return stress
+
+    def extract_random_rank_distribution_params(self, n_neighbors, n_extra_ranks=[]):
+        param_cache_path = os.path.join(os.path.sep, os.getcwd(), 'embedding', 'stress_evaluator', "cache")
+        if len(n_extra_ranks) == 0:
+            file_name = f'{param_cache_path}/{self.n_representative_points}-representative_random_rank_distribution_params.csv'
+            try:
+                params = pd.read_csv(file_name, index_col=0)
+                if float(n_neighbors) in params.index.to_list():
+
+                    return params.loc[n_neighbors]['mean'], params.loc[n_neighbors]['var']
+                else:
+                    mean, var = self.calc_random_rank_distribution_params(n_neighbors)
+                    params.loc[n_neighbors] = [mean, var]
+                    params.to_csv(file_name)
+            except FileNotFoundError:
+                mean, var = self.calc_random_rank_distribution_params(n_neighbors)
+                params = pd.Series([n_neighbors, mean, var], index=['n_neighbors', 'mean', 'var'])
+                params = params.to_frame().T
+                params.to_csv(file_name, index=False)
+
+            return mean, var
+        else:
+            return self.calc_random_rank_distribution_params(n_neighbors, n_extra_ranks)
+
+    def calc_random_rank_distribution_params(self, n_neighbors, n_extra_ranks=[]):
+        n_iter = 1000
+        all_state = np.zeros(n_iter)
+        if len(n_extra_ranks) == 0:
+            for iteration in range(n_iter):
+                one_represent_state = np.zeros(self.n_representative_points)
+                for i in range(self.n_representative_points):
+                    one_represent_state[i] = np.sum(
+                        (np.arange(1, n_neighbors + 1) - np.random.permutation(
+                            np.arange(1, n_neighbors + 1))) ** 2).mean()
+                all_state[iteration] = np.mean(one_represent_state)
+        else:
+            for iteration in range(n_iter):
+                one_represent_state = np.zeros(self.n_representative_points)
+                for i in range(self.n_representative_points):
+                    for j in range(n_neighbors):
+                        one_represent_state[i] += np.sum(
+                            np.abs(np.arange(1, n_neighbors + 1) - np.random.permutation(
+                                np.arange(1, n_neighbors + n_extra_ranks[i, j] + 1))[:n_neighbors]))
+                    one_represent_state[i] = one_represent_state[i] / n_neighbors
+                all_state[iteration] = np.median(one_represent_state)
+
+        return all_state.mean(), np.var(all_state)
 
 
 def choice_representative_indexes(data_points, n_representative_points):
--- embedding/tests/test_stress_evaluator.py
+++ embedding/tests/test_stress_evaluator.py
@@ -12,5 +12,25 @@ def test_kruskal_stress():
     assert evaluator.kruskal(fitted_data.em) > 0
 
 
+def test_local_ranking_stress():
+    actual_data = chooseData('scurve')
+    fitted_data = chooseEmbedder('isomap', actual_data)
+    fitted_data.embed(use_cache=True)
+
+    evaluator = StressEvaluator(actual_data.df, 100)
+    assert evaluator.local_ranking(fitted_data.em, 15, is_z_value=False) > 0
+
+
+def test_middle_ranking_stress():
+    actual_data = chooseData('scurve')
+    fitted_data = chooseEmbedder('isomap', actual_data)
+    fitted_data.embed(use_cache=True)
+
+    evaluator = StressEvaluator(actual_data.df, 100)
+    assert evaluator.middle_ranking(fitted_data.em, 15, is_z_value=False) > 0
+
+
 if __name__ == '__main__':
     test_kruskal_stress()
+    test_local_ranking_stress()
+    test_middle_ranking_stress()
